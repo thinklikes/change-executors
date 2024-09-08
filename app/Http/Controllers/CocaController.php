4bd01971f5b72b4bf205d5efaf4236bca98a4715
Rhys.k <rhys.k.hm@gmail.com>
2024-09-08 13:29:17 +0800

    feat: 加入 controller

---
+++ app/Http/Controllers/CocaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CocaController extends Controller
+{
+    public function drink()
+    {
+        return 'I love Coca!!';
+    }
+}
